8b5e99640d2396559b335fc1a4919493ee2c0f40
Joey Rubin <josemv3@mac.com>
2022-03-22 09:47:52 -0700

    First commit, A-D complete. Have a sorting issue with clicking image and cycling, sometimes it repeats. Gonna add new images and letters.

--- miaTalks2021/ViewController.swift
+++ miaTalks2021/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    var soundTypeSelected = "Human"
+    var soundNameToPlay = "None"
+    
+    @IBOutlet weak var humanButton: UIButton!
+    @IBOutlet weak var robotButton: UIButton!
+    @IBOutlet weak var monsterButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        humanButton.isSelected = true
+        humanButton.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     }
 
-
+    @IBAction func buttonPressedAll(_ sender: UIButton) {
+        
+        soundNameToPlay = sender.currentTitle!
+        let soundNameAndType = soundNameToPlay + soundTypeSelected
+        playSound(soundName: soundNameAndType)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+           
+            sender.alpha = 1.0
+        }
+    }
+    
+    @IBAction func soundTypePressed(_ sender: UIButton) {
+        updateUI()
+        
+        soundTypeSelected = sender.currentTitle!
+        sender.isHighlighted = true
+        sender.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        
+    }
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
+    func updateUI () {
+        humanButton.backgroundColor = UIColor.clear
+        robotButton.backgroundColor = UIColor.clear
+        monsterButton.backgroundColor = UIColor.clear
+    }
 }
 
